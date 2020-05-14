00c694686273bedd9de173c5eac33699217b8b2b
qiuzhanghua <qiuzhanghua@icloud.com>
2020-05-14 08:30:31 +0800

    增加pair的用法示例。

--- src/main/java/cn/com/taiji/talent/may/CountMay.java
+++ src/main/java/cn/com/taiji/talent/may/CountMay.java
@@ -4,9 +4,11 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public class CountMay {
 
-    public int count(int m, int n, ImmutablePair[] traps) {
+    public int count(int m, int n, ImmutablePair<Integer, Integer>[] traps) {
         //请输入您的代码
-
+//        ImmutablePair<Integer, Integer> samplePoint = ImmutablePair.of(3, 4);
+//        int row = samplePoint.left;  // row = 3
+//        int col = samplePoint.right; // col = 4;
 
         return 0;
     }
